406da7aaf1d1a7326393fd0f6ec997b1f1d0b236
ateverz <atevera.rz@gmail.com>
2023-07-31 12:13:21 -0600

    Fix penality parameter

--- uav/src/DILWAC.h
+++ uav/src/DILWAC.h
@@ -12,8 +12,8 @@ class DILWAC
         ~DILWAC();
 
         void setANN(const Eigen::MatrixXf& Lambda_);
-        void setCNN(const int gamma_, const int penality_, Eigen::MatrixXf& GammaC_, float alpha_l, float lamb_l);
-        void setRewardPolicy(float goal_, uint16_t penality_);
+        void setCNN(const int gamma_, const int penality_, Eigen::MatrixXf& GammaC_, float goal, float alpha_l, float lamb_l);
+        //void setRewardPolicy(float goal_, uint16_t penality_);
 
         Eigen::MatrixXf learnDampingInjection(const Eigen::VectorXf& we, const Eigen::Quaternionf& qe, const Eigen::Quaternionf& qep,const Eigen::VectorXf& sq,const Eigen::Quaternionf qd, const Eigen::Quaternionf q, const Eigen::Quaternionf qp, const Eigen::Quaternionf qdp, float delta_t);
 
@@ -25,7 +25,7 @@ class DILWAC
 
     private:
         float* goal;
-        uint16_t* penality;
+        int* penality;
         Eigen::VectorXf* r;
         
 
